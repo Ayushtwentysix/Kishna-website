2a7a4cb3f18258b52311a878e1b3da6cd3682d95
AyushGup26 <gupta.ayush1997@gmail.com>
2018-07-14 04:38:28 +0000

    updated files to be deployed on heroku

--- trek.js
+++ trek.js
@@ -1,4 +1,4 @@
-var express =require("express") 
+var express =require("express") ;
 var app =express();
 // var mongoose = require("mongoose");
 //var methodOverride = require("method-override");
@@ -25,68 +25,68 @@ app.use(express.static("public"));
 // })
 
 
-app.get("/",function(req, res) {
-    res.render("landing.ejs");
-})
+// app.get("/",function(req, res) {
+//     res.render("landing.ejs");
+// })
 
-app.get("/home",function(req, res) {
-    res.render("home.ejs");
-})
-app.get("/trek/treks",function(req,res){
-//   Blogs.find({},function(err,AllBlogs){
-//       if(err){
-//           console.log("ERROR FOUND:"+err);
-//       } else {
-//           res.render("show_trek.ejs",{blogs:AllBlogs});
-//       }
-//   })
-    res.render("show_trek.ejs");
-});
+// app.get("/home",function(req, res) {
+//     res.render("home.ejs");
+// })
+// app.get("/trek/treks",function(req,res){
+// //   Blogs.find({},function(err,AllBlogs){
+// //       if(err){
+// //           console.log("ERROR FOUND:"+err);
+// //       } else {
+// //           res.render("show_trek.ejs",{blogs:AllBlogs});
+// //       }
+// //   })
+//     res.render("show_trek.ejs");
+// });
 
-app.get("/treks/t1",function(req, res) {
-   res.render("t1.ejs"); 
-});
+// app.get("/treks/t1",function(req, res) {
+//    res.render("t1.ejs"); 
+// });
 
-app.get("/treks/t2",function(req, res) {
-   res.render("t2.ejs"); 
-})
-app.get("/treks/t3",function(req, res) {
-   res.render("t3.ejs"); 
-})
-app.get("/treks/t4",function(req, res) {
-   res.render("t4.ejs"); 
-})
-app.get("/treks/t4A",function(req, res) {
-   res.render("t4A.ejs"); 
-})
-app.get("/treks/t5",function(req, res) {
-   res.render("t5.ejs"); 
-})
-app.get("/treks/t5A",function(req, res) {
-   res.render("t5A.ejs"); 
-})
-app.get("/treks/t6",function(req, res) {
-   res.render("t6.ejs"); 
-})
-app.get("/treks/t6A",function(req, res) {
-   res.render("t6A.ejs"); 
-})
-app.get("/trek/leisure",function(req, res) {
-   res.render("leisure.ejs"); 
-});
+// app.get("/treks/t2",function(req, res) {
+//    res.render("t2.ejs"); 
+// })
+// app.get("/treks/t3",function(req, res) {
+//    res.render("t3.ejs"); 
+// })
+// app.get("/treks/t4",function(req, res) {
+//    res.render("t4.ejs"); 
+// })
+// app.get("/treks/t4A",function(req, res) {
+//    res.render("t4A.ejs"); 
+// })
+// app.get("/treks/t5",function(req, res) {
+//    res.render("t5.ejs"); 
+// })
+// app.get("/treks/t5A",function(req, res) {
+//    res.render("t5A.ejs"); 
+// })
+// app.get("/treks/t6",function(req, res) {
+//    res.render("t6.ejs"); 
+// })
+// app.get("/treks/t6A",function(req, res) {
+//    res.render("t6A.ejs"); 
+// })
+// app.get("/trek/leisure",function(req, res) {
+//    res.render("leisure.ejs"); 
+// });
 
-app.get("/trek/rafting",function(req, res) {
-   res.render("rafting.ejs"); 
-});
+// app.get("/trek/rafting",function(req, res) {
+//    res.render("rafting.ejs"); 
+// });
 
 
-app.get("/trek/paragliding",function(req, res) {
-   res.render("paragliding.ejs"); 
-});
+// app.get("/trek/paragliding",function(req, res) {
+//    res.render("paragliding.ejs"); 
+// });
 
-app.get("/trek/guest",function(req, res) {
-   res.render("guest.ejs"); 
-});
+// app.get("/trek/guest",function(req, res) {
+//    res.render("guest.ejs"); 
+// });
 // app.get("/trek/new",function(req,res){
 //     res.render("trekNew.ejs");
 // })
@@ -114,6 +114,74 @@ app.get("/trek/guest",function(req, res) {
 //   })
 // });
 
+app.get("/",function(req, res) {
+    res.render("landing.ejs");
+})
+
+app.get("/Camps-Activities",function(req, res) {
+    res.render("home.ejs");
+})
+
+app.get("/trek/treks",function(req,res){
+//   Blogs.find({},function(err,AllBlogs){
+//       if(err){
+//           console.log("ERROR FOUND:"+err);
+//       } else {
+//           res.render("show_trek.ejs",{blogs:AllBlogs});
+//       }
+//   })
+    res.render("show_trek.ejs");
+});
+
+app.get("/treks/Mandi-to-PrasharLake-Bajaura-Trek",function(req, res) {
+   res.render("t0.ejs"); 
+});
+
+
+app.get("/treks/Mandi-to-PrasharLake-Trek",function(req, res) {
+   res.render("t1.ejs"); 
+});
+
+app.get("/treks/Bagi-PrasharLake-Trek",function(req, res) {
+   res.render("t2.ejs"); 
+})
+app.get("/treks/Bagi-PrasharLake-MchhodriMata-Trek",function(req, res) {
+   res.render("t4.ejs"); 
+})
+app.get("/treks/PrasharLake-MchhodriMata-Trek",function(req, res) {
+   res.render("t4A.ejs"); 
+})
+app.get("/treks/Bagi-PrasharLake-TungaMata-Trek",function(req, res) {
+   res.render("t5.ejs"); 
+})
+app.get("/treks/PrasharLake-TungaMata-Trek",function(req, res) {
+   res.render("t5A.ejs"); 
+})
+app.get("/treks/Bagi-PrasharLake-ShehlaMata-Trek",function(req, res) {
+   res.render("t6.ejs"); 
+})
+app.get("/treks/PrasharLake-ShehlaMata-Trek",function(req, res) {
+   res.render("t6A.ejs"); 
+})
+// app.get("/treks/t6A",function(req, res) {
+//    res.render("t6A.ejs"); 
+// })
+app.get("/trek/leisure",function(req, res) {
+   res.render("leisure.ejs"); 
+});
+
+app.get("/trek/rafting",function(req, res) {
+   res.render("rafting.ejs"); 
+});
+
+
+app.get("/trek/paragliding",function(req, res) {
+   res.render("paragliding.ejs"); 
+});
+
+app.get("/trek/guest-house",function(req, res) {
+   res.render("guest.ejs"); 
+});
 
 
 
